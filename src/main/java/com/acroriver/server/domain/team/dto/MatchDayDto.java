665b4d6e343a10cc35fb98fbcf4381060763de6d
Hwanu Jung <sossont1@gmail.com>
2022-03-04 15:55:56 +0900

    DTO 세팅

--- src/main/java/com/acroriver/server/domain/team/dto/MatchDayDto.java
+++ src/main/java/com/acroriver/server/domain/team/dto/MatchDayDto.java
@@ -3,12 +3,14 @@ package com.acroriver.server.domain.team.dto;
 import com.acroriver.server.domain.team.entity.PlayMatch;
 import com.acroriver.server.domain.team.entity.enums.MatchState;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 public class MatchDayDto {
     private Long id;
     private LocalDate matchDate;
@@ -16,5 +18,10 @@ public class MatchDayDto {
     private MatchState result;
     private List<PlayMatch> playMatchList = new ArrayList<>();
 
-
+    public MatchDayDto(Long id, LocalDate matchDate, String awayName, MatchState result) {
+        this.id = id;
+        this.matchDate = matchDate;
+        this.awayName = awayName;
+        this.result = result;
+    }
 }
